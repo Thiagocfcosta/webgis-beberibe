aab6cbc0567c3a35ef750bec952c75ad091fec20
Thiagocfcosta <thiagocfcosta@gmail.com>
2026-04-30 18:35:18 -0300

    fix: closing bracket for JSX condition

--- src/components/SavedMapsDrawer.js
+++ src/components/SavedMapsDrawer.js
@@ -715,6 +715,7 @@ export default function SavedMapsDrawer({
                         );
                       })}
                     </div>
+                    )}
                   </div>
                 )}
 
